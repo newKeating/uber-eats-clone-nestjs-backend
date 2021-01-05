eda45b863bbecb233229f9c62bc877e702dec1f9
Peter Kim <kroits09@gmail.com>
2021-01-05 13:40:30 +0900

    Update jwt middleware

--- src/jwt/jwt.middleware.ts
+++ src/jwt/jwt.middleware.ts
@@ -1,14 +1,24 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { NextFunction, Request, Response } from 'express';
 import { JwtService } from './jwt.service';
+import { UsersService } from '../users/users.service';
 
 @Injectable()
 export class JwtMiddleware implements NestMiddleware {
-  constructor(private readonly jwtService: JwtService) {}
-  use(req: Request, res: Response, next: NextFunction) {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly usersService: UsersService,
+  ) {}
+  async use(req: Request, res: Response, next: NextFunction) {
     if ('x-jwt' in req.headers) {
       const token = req.headers['x-jwt'];
       const decoded = this.jwtService.verify(token.toString());
+      if (typeof decoded === 'object' && decoded.hasOwnProperty('id')) {
+        try {
+          const user = await this.usersService.findById(decoded['id']);
+          req['user'] = user;
+        } catch (e) {}
+      }
     }
     next();
   }
