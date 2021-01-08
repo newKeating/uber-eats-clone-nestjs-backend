b9b3a601ce5b416d7a7b5966153a6c681842b3d1
Peter Kim <kroits09@gmail.com>
2021-01-08 13:33:59 +0900

    Add userService test

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -79,20 +79,18 @@ export class UserService {
     } catch (error) {
       return {
         ok: false,
-        error,
+        error: "Can't log user in",
       };
     }
   }
 
   async findById(id: number): Promise<UserProfileOutput> {
     try {
-      const user = await this.users.findOne({ id });
-      if (user) {
-        return {
-          ok: true,
-          user: user,
-        };
-      }
+      const user = await this.users.findOneOrFail({ id });
+      return {
+        ok: true,
+        user: user,
+      };
     } catch (error) {
       return { ok: false, error: 'User Not Found' };
     }
@@ -141,7 +139,7 @@ export class UserService {
       }
       return { ok: false, error: 'Verification not found.' };
     } catch (error) {
-      return { ok: false, error };
+      return { ok: false, error: 'Could not verify email.' };
     }
   }
 }
